84bde2c991442b2006d905e48ef4cd5b6e0febe3
Ankit Anand <40232229+ankitrrock@users.noreply.github.com>
2020-05-10 17:40:47 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from appmart import views as app
+
+
+urlpatterns = [
+	path('', app.index, name="index"),
+	path('registerPage/', app.registerPage, name="registerPage"),
+	path('loginPage/', app.loginPage, name="loginPage"),
+	path('logout_django/', app.logout_django, name="logout_django"),
+	path('about', app.about, name="about"),
+	path('checkout', app.checkout, name="checkout"),
+	path('dresses', app.dresses, name="dresses"),
+	path('faq', app.faq, name="faq"),
+	path('jeans', app.jeans, name="jeans"),
+	path('mail', app.mail, name="mail"),
+	path('products', app.products, name="products"),
+	path('salwars', app.salwars, name="salwars"),
+	path('sandals', app.sandals, name="sandals"),
+	path('sarees', app.sarees, name="sarees"),
+	path('shirts', app.shirts, name="shirts"),
+	path('shirtcodes', app.shirtcodes, name="shirtcodes"),
+	path('single', app.single, name="single"),
+	path('skirts', app.skirts, name="skirts"),
+	path('sweaters', app.sweaters, name="sweaters"),
+
+
+
+]
---
+++ views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
+
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('loginPage')
+
+	context = {'form': form}
+	return render(request, 'registerPage', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('index')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+	context = {}
+	return render(request, 'login.html', context)
+
+def logout_django(request):
+	logout(request)
+	return redirect('loginPage')
+
+@login_required(login_url='loginPage')
+def index(request):
+	return render(request,'index.html')
+
+
+@login_required(login_url='loginPage')
+def about(request):
+	return render(request,'about.html')
+
+
+@login_required(login_url='loginPage')
+def checkout(request):
+	return render(request,'checkout.html')
+
+
+@login_required(login_url='loginPage')
+def dresses(request):
+	return render(request,'dresses.html')
+
+@login_required(login_url='loginPage')
+def faq(request):
+	return render(request,'faq.html')
+
+
+@login_required(login_url='loginPage')
+def jeans(request):
+	return render(request,'jeans.html')
+
+
+@login_required(login_url='loginPage')
+def mail(request):
+	return render(request,'mail.html')
+
+
+@login_required(login_url='loginPage')
+def products(request):
+	return render(request,'products.html')
+
+
+@login_required(login_url='loginPage')
+def salwars(request):
+	return render(request,'salwars.html')
+
+
+@login_required(login_url='loginPage')
+def sandals(request):
+	return render(request,'sandals.html')
+
+
+@login_required(login_url='loginPage')
+def sarees(request):
+	return render(request,'sarees.html')
+
+
+@login_required(login_url='loginPage')
+def shirts(request):
+	return render(request,'shirts.html')
+
+
+@login_required(login_url='loginPage')
+def shirtcodes(request):
+	return render(request,'shirtcodes.html')
+
+
+@login_required(login_url='loginPage')
+def single(request):
+	return render(request,'single.html')
+
+
+@login_required(login_url='loginPage')
+def skirts (request):
+	return render(request,'skirts.html')
+
+
+@login_required(login_url='loginPage')
+def sweaters (request):
+	return render(request,'sweaters.html')
+
+
+
+
+
